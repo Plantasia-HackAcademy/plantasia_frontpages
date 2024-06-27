ef0254611b54797eaffc539844d79da8d4a7611c
Laura Pérez Betarte <lmperezbetarte@gmail.com>
2024-06-27 15:34:47 -0300

    Avances en home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,11 +5,12 @@ function Home() {
   return (
     <>
       <Navbar />
-      <div className="home-banner">
-        <div className="container banner-container">
+      <div className="home-banner d-flex align-items-end">
+        <div className="container banner-container d-flex flex-column">
           {" "}
-          <span className="d-block galadali-bold">Growing</span>
-          <span className="d-block galadali-bold">Happiness</span>
+          <span className="galadali-bold">Growing</span>
+          <span className="galadali-bold">Happiness</span>
+        <button className="banner-button">Shop all our plants</button>
         </div>
       </div>
     </>
