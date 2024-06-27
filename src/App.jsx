28d3283f7d068cff47664943036cc23c942ccdbd
denisse2911 <134969268+denisse2911@users.noreply.github.com>
2024-06-27 15:16:31 -0300

    Rutas login signUp

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Home from "./pages/Home";
 import ProductPage from "./pages/ProductPage";
-
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 import ErrorPage from "./pages/ErrorPage";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
@@ -13,6 +14,14 @@ function App() {
     {
       path: "/product",
       element: <ProductPage />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <SignUp />,
     },
     {
       path: "*",
