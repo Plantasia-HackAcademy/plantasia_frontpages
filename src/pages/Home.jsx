1ec83cfe561ccd6ce9a3fd17a5c72c159ddae366
Feder17 <federicarodriguez@gmail.com>
2024-06-28 20:27:11 +0100

    Cards flexbox

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,28 +8,45 @@ function Home() {
       <div className="home-banner d-flex align-items-end">
         <div className="container banner-container d-flex flex-column justify-content-center p-0">
           <h1 className="galadali-bold banner-title">Growing Happiness</h1>
-          <p className="banner-description proxima-nova-regular p-0">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora beatae nihil mollitia nulla repudiandae in labore debitis blanditiis porro quaerat?</p>
-          <a href=""className="banner-button">SHOP NOW</a>
+          <p className="banner-description proxima-nova-regular p-0">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora beatae nihil mollitia
+            nulla repudiandae in labore debitis blanditiis porro quaerat?
+          </p>
+          <a href="" className="banner-button">
+            SHOP NOW
+          </a>
         </div>
       </div>
 
       <section className="section-2">
-        <div className="container d-flex">
-          <div className="section-2-cards">
+        <div className="container cards-container d-flex p-0">
+          <div className="section-2-cards d-flex">
             <img src="../images/photo-plants.png" className="object-fit-cover w-100" alt="" />
-            <span className="card-title-plants galadali-regular">Plants</span>
+            <span className="card-title galadali-regular">Plants</span>
             <a className="btn button-see-all proxima-nova-regular" href="">
               See all
             </a>
           </div>
-          <div className="section-2-cards text-center">
+          <div className="section-2-cards d-flex">
             <img src="../images/photo-pots.png" className="object-fit-cover w-100" alt="" />
+            <span className="card-title galadali-regular">Pots</span>
+            <a className="btn button-see-all proxima-nova-regular" href="">
+              See all
+            </a>
           </div>
-          <div className="section-2-cards text-center bg-secondary">
+          <div className="section-2-cards d-flex">
             <img src="../images/photo-care.png" className="object-fit-cover w-100" alt="" />
+            <span className="card-title galadali-regular">Care</span>
+            <a className="btn button-see-all proxima-nova-regular" href="">
+              See all
+            </a>
           </div>
-          <div className="section-2-cards text-center bg-success">
+          <div className="section-2-cards d-flex">
             <img src="../images/photo-accessories.png" className="object-fit-cover w-100" alt="" />
+            <span className="card-title galadali-regular">Accesories</span>
+            <a className="btn button-see-all proxima-nova-regular" href="">
+              See all
+            </a>
           </div>
         </div>
         {/* <div className="container d-flex">
