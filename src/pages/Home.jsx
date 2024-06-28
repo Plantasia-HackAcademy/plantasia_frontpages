84172fa2419fe1698284fb8f328148a14ecc4dbf
Feder17 <federicarodriguez@gmail.com>
2024-06-28 19:18:32 +0100

    Cards Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,7 +28,41 @@ function Home() {
           </div>
         </div>
       </div>
-      <section className="section-2"></section>
+
+      <section className="section-2">
+        <div className="container d-flex">
+          <div className="section-2-cards">
+            <img src="../images/photo-plants.png" className="object-fit-cover w-100" alt="" />
+            <span className="card-title-plants galadali-regular">Plants</span>
+            <a className="btn button-see-all proxima-nova-regular" href="">
+              See all
+            </a>
+          </div>
+          <div className="section-2-cards text-center">
+            <img src="../images/photo-pots.png" className="object-fit-cover w-100" alt="" />
+          </div>
+          <div className="section-2-cards text-center bg-secondary">
+            <img src="../images/photo-care.png" className="object-fit-cover w-100" alt="" />
+          </div>
+          <div className="section-2-cards text-center bg-success">
+            <img src="../images/photo-accessories.png" className="object-fit-cover w-100" alt="" />
+          </div>
+        </div>
+        {/* <div className="container d-flex">
+          <div className="find-the-one-div d-flex flex-column">
+            <span>Find the one for you</span>
+            <span>
+              Are you looking for someone to spend time outdoors with? Or some company to watch your
+              favorite series? Maybe you’re just looking for someone who gets along with your pet.
+              In any case, we have the one for you!
+            </span>
+            <a href="">Discover our plants -- </a>
+          </div>
+          <div className="photo-find">
+            <img src="../images/photo-accessories.png" alt="" />
+          </div>
+        </div> */}
+      </section>
     </>
   );
 }
